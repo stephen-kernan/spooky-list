9257ba9768d4d6d75069a005b0d1043520c52576
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2023-08-08 22:11:02 -0500

    LTC-43: Made NavBar Responsive (#14)
    * LTC-43: Made NavBar Responsive
    
    * LTC-43 -- Fix Tests

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,16 @@
 'use client'
 import React from 'react'
+import { MenuRounded } from '@mui/icons-material'
 import styles from './NavBar.module.scss'
 import { Button } from '@/components/Button/Button'
+import { AppBar, Box, Drawer, IconButton } from '@mui/material'
 
 interface NavBarProps {
   currentPage: string
 }
 
 export const NavBar = ({ currentPage }: NavBarProps): JSX.Element => {
+  const [drawerOpen, setDrawerOpen] = React.useState(false)
   const navLinks = [
     { link: '/dashboard', text: 'Dashboard' },
     { link: '/about', text: 'About' },
@@ -18,22 +21,76 @@ export const NavBar = ({ currentPage }: NavBarProps): JSX.Element => {
     window.location.href = '/sign-up'
   }
 
+  const toggleDrawer = (open: boolean): void => {
+    setDrawerOpen(open)
+  }
+
   return (
-      <nav className={styles.navBar} data-testid={'nav-bar'}>
-          <h1 className={styles.navTitle}>Spooky</h1>
-          {navLinks.map(link => (
-              <a
-                  data-testid={`nav-link-${link.text}`}
-                  className={`${styles.navText} ${(link.text === currentPage) ? styles.active : ''}`}
-                  href={link.link}
-                  key={link.link}
+      <>
+          <AppBar component={'nav'} className={styles.navBar} data-testid={'nav-bar'}>
+              <h1 className={styles.navTitle}>Spooky</h1>
+              <Box className={styles.navLinks} sx={{ display: { xs: 'none', md: 'flex' } }}>
+                  {navLinks.map(link => (
+                      <a
+                          data-testid={`nav-link-${link.text}`}
+                          className={`${styles.navText} ${(link.text === currentPage) ? styles.active : ''}`}
+                          href={link.link}
+                          key={link.link}
+                      >
+                          {link.text}
+                      </a>
+                  ))}
+              </Box>
+              <Box className={styles.buttonWrapper} sx={{ display: { xs: 'none', md: 'block' } }}>
+                  <Button text={'Sign Up'} onClick={redirectToSignUp} />
+              </Box>
+              <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                  onClick={() => { toggleDrawer(true) }}
+                  sx={{ ml: 'auto', display: { md: 'none' } }}
               >
-                  {link.text}
-              </a>
-          ))}
-          <div className={styles.buttonWrapper}>
-              <Button text={'Sign Up'} onClick={redirectToSignUp} />
-          </div>
-      </nav>
+                  <MenuRounded />
+              </IconButton>
+
+          </AppBar>
+          <Box component="nav">
+              <Drawer
+                  // container={container}
+                  className={styles.drawer}
+                  variant="temporary"
+                  open={drawerOpen}
+                  onClose={() => { toggleDrawer(false) }}
+                  ModalProps={{
+                    keepMounted: true // Better open performance on mobile.
+                  }}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                    '& .MuiDrawer-paper': {
+                      boxSizing: 'border-box',
+                      width: '70%',
+                      paddingInline: '2rem',
+                      backgroundColor: 'var(--grimace)',
+                      borderRight: '3px solid var(--black-cat)'
+                    }
+                  }}
+              >
+                  <h1 className={styles.navTitle}>Spooky</h1>
+                  {navLinks.map(link => (
+                      <a
+                          className={`${styles.navText} ${(link.text === currentPage) ? styles.active : ''}`}
+                          href={link.link}
+                          key={link.link}
+                      >
+                          {link.text}
+                      </a>
+                  ))}
+                  <Box className={styles.buttonWrapper} >
+                      <Button text={'Sign Up'} onClick={redirectToSignUp} />
+                  </Box>
+              </Drawer>
+          </Box>
+      </>
   )
 }
